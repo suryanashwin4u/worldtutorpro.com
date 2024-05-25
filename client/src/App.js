6fc9fa71b783658f3931195ae70429d9185a3911
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2024-05-25 22:20:39 +0530

    changes 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,21 @@
+// src/App.js
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:5000')
-      .then(response => {
-        setMessage(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the data!', error);
-      });
+    axios.get('http://localhost:5000/')
+      .then(response => setMessage(response.data))
+      .catch(error => console.error('There was an error!', error));
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MERN Stack Project</h1>
-        <p>{message}</p>
-      </header>
+    <div>
+      <h1>{message}</h1>
     </div>
   );
-}
+};
 
 export default App;
